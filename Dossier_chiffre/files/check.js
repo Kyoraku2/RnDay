4d7a4a16e43296ee2e9664899d105b8b7b1424fa
Kyoraku2 <58563503+Kyoraku2@users.noreply.github.com>
2021-02-24 13:46:27 +0100

    Add escape game

---
+++ Dossier_chiffre/files/check.js
@@ -0,0 +1,5 @@
+function fct0() {return document.getElementById("m2Q5").value;}
+function fct1(s){var j = "";for(let i = 0; i < s.length; i++){j += s.charCodeAt(i).toString("a5drf3cf.frz+7f ".length);}return j;}
+function fct2(c){return fct1(c)=="594f55415245544845424f5353";}                                                   
+function fct3(){if(fct2(fct0())){alert(fct4("434f4e54454e55204445434849464652c90a566f7573206176657a2073617576e9206c657320747261766175782064752070726f6665737365757220210a2d202d202d204d495353494f4e204143434f4d504c4945202d202d202d"));}else{alert(fct4("434cc920494e434f525245435445"));}}
+function fct4(s){var j = "";for(let i = 0; i < s.length; i+=2){j+=String.fromCharCode("0x"+s[i]+s[i+1]);}return j;}
